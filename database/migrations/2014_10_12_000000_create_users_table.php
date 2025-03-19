ecf13586d623a9826ea7bcb95b4b49eb1eb9bf51
namph-kozocom <namph@kdhm-solutions.com>
2025-03-19 11:16:33 +0700

    feat: config connect database and migrate table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('phone', 11)->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('gender', 10)->nullable();
+            $table->string('address')->nullable();
             $table->timestamps();
         });
     }
